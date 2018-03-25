2b54c6b520dc39ab8809defacfb66cedca55ff70
James Ridgway <myself@james-ridgway.co.uk>
2018-03-25 11:44:07 +0100

    Use datafile for tests

--- tests/t00_check.sh
+++ tests/t00_check.sh
@@ -2,6 +2,9 @@
 # shellcheck disable=SC2016
 set -e
 
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+DATA_FILE="${DIR}/data-file"
+
 export test_description="Testing pass pwned check."
 
 source ./setup
@@ -11,34 +14,34 @@ test_export "check"
 
 test_expect_success 'Test "check" fails for unknown passfile' '
 	pass init $KEY1 &&
-	! pass pwned check unknown-pass'
+	! pass pwned check -f '"${DATA_FILE}"' unknown-pass'
 
 test_expect_success 'Test "check" command finds bad password (error message)' '
 	pass init $KEY1 &&
 	echo -n "P@ssw0rd" | pass insert -e cred1 &&
-	[ "$(pass pwned check cred1)" = "The password for '"'"'cred1'"'"' has appeared 47205 times in data breaches!" ]'
+	[ "$(pass pwned check -f '"${DATA_FILE}"' cred1)" = "The password for '"'"'cred1'"'"' has appeared 47205 times in data breaches!" ]'
 
 test_expect_success 'Test "check" command finds bad password (exit code)' '
 	pass init $KEY1 &&
 	echo -n "P@ssw0rd" | pass insert -e cred1 &&
-	! pass pwned check cred1'
+	! pass pwned check -f '"${DATA_FILE}"' cred1'
 
 
 test_expect_success 'Test "check" command determines password (message)' '
 	pass init $KEY1 &&
 	echo -n "5uP3r$3cUr3/" | pass insert -e cred2 &&
-	[ "$(pass pwned check cred2)" = "The password for '"'"'cred2'"'"' has not appeared in any data breaches." ]'
+	[ "$(pass pwned check -f '"${DATA_FILE}"' cred2)" = "The password for '"'"'cred2'"'"' has not appeared in any data breaches." ]'
 
 test_expect_success 'Test "check" command determines good password (exit code)' '
 	pass init $KEY1 &&
 	echo -n "5uP3r$3cUr3/" | pass insert -e cred2 &&
-	pass pwned check cred2'
+	pass pwned check -f '"${DATA_FILE}"' cred2'
 
 
 test_expect_success 'Test "check" command ignores second line and fails on the bad password' '
 	pass init $KEY1 &&
 	echo -e "P@ssw0rd\nSecond line ignored" | pass insert -e cred1 &&
-	[ "$(pass pwned check cred1)" = "The password for '"'"'cred1'"'"' has appeared 47205 times in data breaches!" ]'
+	[ "$(pass pwned check -f '"${DATA_FILE}"' cred1)" = "The password for '"'"'cred1'"'"' has appeared 47205 times in data breaches!" ]'
 	
 
 test_done
